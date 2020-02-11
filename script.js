6bf66807e0aa35fae49237c8f2fac3ef757e327c
jab_tx <javier.a.baragan92@gmail.com>
2020-02-11 16:13:34 -0600

    Working UV Index

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   var citiesLocal = JSON.parse(window.localStorage.getItem('cityHistory')) || [] 
   renderButtons()
 
-
   //Reloading previous search using .get(-1) to grab last result
   $(document).ready(function renderLastCity(){
     var cityreload= $(citiesLocal).get(-1);
@@ -20,10 +19,10 @@ $(document).ready(function() {
     //Top module script, displays today info
     $.ajax({url: queryURL, method: "GET"}).then(function (response) {
       // console.log(response)
-          $("#city").html("<h1>Weather in " + response.name + "</h1>");
-          $("#temp").html("<h2>" + Math.ceil (response.main.temp) + " degrees" + "</h2>");
-          $("#cond").html("<h3>" + response.weather[0].main + "</h3>");
-          $("#humidity").html("<h2>"+ "Humidity " + Math.ceil (response.main.humidity) + "</h2>");
+          $("#city").html("<h1>Weather in " + response.name + " right now" + "</h1>");
+          $("#temp").html("<h2>" + Math.ceil (response.main.temp) + " °f " + "</h2>");
+          $("#cond").html("<h4>" + response.weather[0].main + "</h4>");
+          $("#humidity").html("<h2>"+ "Humidity " + Math.ceil (response.main.humidity) + "% </h2>");
           $("#wind").html(" <h2>" + Math.ceil(response.wind.speed) + " mph" + "</h2>");
           $("#wicon").html("<img src="+ "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png" +">")
     
@@ -34,24 +33,29 @@ $(document).ready(function() {
     
     // UV Index api, then displays with top module
       $.ajax({url: uvindexURL, method: "GET"}).then(function (response) {
-          $("#uvIndexValue").html(" <h2>" + "UV Index: "+ response.value + "</h2>");
-
+          $("#uvIndexValue").html(" <h2>" + "UV Index: "+"<p id= 'uvIndexValueNumber'>"+ response.value + "</p>"+"</h2>");
+          var index = parseInt($("#uvIndexValueNumber").text());  
+          if (index < 3)
+              $('#uvIndexValueNumber').css('color', 'green');
+             else{ (6 > index > 3 )
+              $('#uvIndexValueNumber').css('color', 'yellow');
+              if (index > 6)
+              $('#uvIndexValueNumber').css('color', 'maroon');
+            }
         })
       })
 
     //Bottom module script, displays 5 day forecast info
     $.ajax({url: query5dayURL, method: "GET"}).then(function (response) {
 
-      $("#forcastday1").html("<p>" +"Tomorrow"+"</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png>" + "<p>"+ "Hi "  + Math.ceil(response.list[0].main.temp_max) + " / Lo "+ Math.ceil(response.list[0].main.temp_min)+ "</p>" + "<p>"+"Humidity "+ response.list[0].main.humidity + "%"+"</p>");
-      $("#forcastday2").html("<p>" + response.list[8].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png>" + "<p>"+ "Hi "  + Math.ceil(response.list[8].main.temp_max)  +  "</p>" + "<p>"+"Humidity " + response.list[8].main.humidity + "%"+"</p>");
-      $("#forcastday3").html("<p>" + response.list[16].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png>" + "<p>"+ "Hi " + Math.ceil(response.list[16].main.temp_max) + "</p>" + "<p>"+"Humidity "  + response.list[16].main.humidity + "%"+"</p>");
-      $("#forcastday4").html("<p>" + response.list[24].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png>" +"<p>" + "Hi "+ Math.ceil(response.list[24].main.temp_max) +  "</p>" + "<p>"+"Humidity " + response.list[24].main.humidity + "%"+"</p>");
-      $("#forcastday5").html("<p>" + response.list[32].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png>" + "<p>"+ "Hi " + Math.ceil(response.list[32].main.temp_max) +  "</p>" + "<p>"+"Humidity " + response.list[32].main.humidity + "%"+"</p>");         
+      $("#forcastday1").html("<p>" +"Tomorrow"+"</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png>" + "<p>"+ "Hi "  + Math.ceil(response.list[0].main.temp_max) + " °f  / Lo "+ Math.ceil(response.list[0].main.temp_min)+ " °f </p>" + "<p>"+"Humidity "+ response.list[0].main.humidity + "%"+"</p>");
+      $("#forcastday2").html("<p>" + response.list[8].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png>" + "<p>"+ "Hi "  + Math.ceil(response.list[8].main.temp_max)  +  " °f </p>" + "<p>"+"Humidity " + response.list[8].main.humidity + "%"+"</p>");
+      $("#forcastday3").html("<p>" + response.list[16].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png>" + "<p>"+ "Hi " + Math.ceil(response.list[16].main.temp_max) + " °f </p>" + "<p>"+"Humidity "  + response.list[16].main.humidity + "%"+"</p>");
+      $("#forcastday4").html("<p>" + response.list[24].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png>" +"<p>" + "Hi "+ Math.ceil(response.list[24].main.temp_max) +  " °f </p>" + "<p>"+"Humidity " + response.list[24].main.humidity + "%"+"</p>");
+      $("#forcastday5").html("<p>" + response.list[32].dt_txt + "</p>" + "<img src="+ "http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png>" + "<p>"+ "Hi " + Math.ceil(response.list[32].main.temp_max) +  " °f </p>" + "<p>"+"Humidity " + response.list[32].main.humidity + "%"+"</p>");         
       
     })
   }
-
-
   // Search function, click search to execute
   $("#searchCity").on("click", function (event) { 
     var city = $("#citySearch-input").val();
